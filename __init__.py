5993b4a9d2b5cb3b2c39b326a62fd91adeab5e84
unknown <typ3ys.xz@gmail.com>
2018-04-01 17:15:26 +0900

    Replace tab to 4 space

--- __init__.py
+++ __init__.py
@@ -16,63 +16,69 @@ bl_info = { \
     'wiki_url': 'https://github.com/sus-unn/bl2d',
     'support': 'COMMUNITY',
     'category': 'Animation'}
-	
+    
 class View3DPanel:
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
 
 
-class VIEW3D_PT_tools_bl2d(View3DPanel, Panel):
-	'''
-	bl2d panel class
-	'''
-	bl_category = "BL2D"
-	bl_label = "BL2D"
-	bl_context = "objectmode"
-	
-	def draw(self, context):
-		global bl2d_icons
-		
-		layout = self.layout
-		
-		col = layout.column()
-		col.label(text = "BL2D", icon_value = bl2d_icons["custom_icon"].icon_id)
-		
-		box = layout.box()
-		box.label("Info")
-		
-		col=layout.column()
-		
-	
-	
+class VIEW3D_PT_tools_bl2d_info(View3DPanel, Panel):
+    '''
+    bl2d panel class
+    '''
+    bl_category = "BL2D"
+    bl_label = "BL2D"
+    bl_context = "objectmode"
+    
+    def draw(self, context):
+        global bl2d_icons
+        
+        layout = self.layout
+        
+        col = layout.column()
+        col.label(text = "BL2D", icon_value = bl2d_icons["custom_icon"].icon_id)
+        
+        box = layout.box()
+        box.label("Info")
+        
+        col=layout.column()
+        
+class VIEW3D_PT_tools_bl2d_info(View3DPanel, Panel):
+    '''
+    bl2d panel class
+    '''
+    bl_category = "BL2D"
+    bl_label = "BL2D"
+    bl_context = "objectmode"
+    
 
-'''	
+'''    
 class SetTimeText(bpy.types.Operator):
 
-	bl_label = "SetTimeText"
-	bl_idname = "object.settimetext"
-	def execute(self, context):
-		bpy.data.objects["Text_Time_Second"].data.body = str(bpy.data.scenes["Main"].frame_end//bpy.data.scenes['Main'].render.fps)
-		bpy.data.objects["Text_Time_Frame"].data.body = str(bpy.data.scenes["Main"].frame_end%bpy.data.scenes['Main'].render.fps)
-		return {'RUNNING_MODAL'}
+    bl_label = "SetTimeText"
+    bl_idname = "object.settimetext"
+    def execute(self, context):
+        bpy.data.objects["Text_Time_Second"].data.body = str(bpy.data.scenes["Main"].frame_end//bpy.data.scenes['Main'].render.fps)
+        bpy.data.objects["Text_Time_Frame"].data.body = str(bpy.data.scenes["Main"].frame_end%bpy.data.scenes['Main'].render.fps)
+        return {'RUNNING_MODAL'}
 
 '''
 bl2d_icons = None
 
-	
+    
 def register():
-	global bl2d_icons
-	bl2d_icons = bpy.utils.previews.new()
-	script_file = os.path.realpath(__file__)
-	bl2d_icons.load("custom_icon", os.path.join(os.path.dirname(script_file),"icons","bl2d_ico.png"), 'IMAGE')
-	bpy.utils.register_module(__name__)
-	
-	
+    global bl2d_icons
+    bl2d_icons = bpy.utils.previews.new()
+    script_file = os.path.realpath(__file__)
+    bl2d_icons.load("custom_icon", os.path.join(os.path.dirname(script_file),"icons","bl2d_ico.png"), 'IMAGE')
+    bpy.utils.register_module(__name__)
+    
+    
 def unregister():
-	global custom_icons
-	bpy.utils.previews.remove(bl2d_icons)
-	bpy.utils.unregister_module(__name__)
-	
+    global custom_icons
+    bpy.utils.previews.remove(bl2d_icons)
+    bpy.utils.unregister_module(__name__)
+    
 if __name__ == "__main__":
-	register()
+    register()
 
