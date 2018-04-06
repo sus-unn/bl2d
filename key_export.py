d1e81ab4c995947fa878cf8915342c657f7e1764
sus-unn <36620824+sus-unn@users.noreply.github.com>
2018-04-06 19:51:16 +0900

    move key_export outside operator class

--- key_export.py
+++ key_export.py
@@ -2,7 +2,33 @@ import bpy
 import os
 from . import bl2d
 
-
+def key_export(gp_layer_name):
+            scene.frame_set(scene.frame_start-1)
+            
+            key_count_blend = 0
+            key_count_sheet = 1
+            
+            while True:
+                if bpy.ops.screen.keyframe_jump(key_jump_next) == {'CANCELLED'}:
+                    break
+                    
+                key_count_str = str(key_count_sheet)
+                
+                if (key_count_sheet < 0) or (key_count_sheet > 9999):
+                    print("Error in key_export: too much keyframes. Maximum count of each cel's keyframe is 9999.")
+                
+                if(key_count_sheet < 10):
+                    key_count_str = "000" + key_count_str
+                if(10 <= key_count_sheet < 100):
+                    key_count_str = "00" + key_count_str
+                if(100 <= key_count_sheet < 1000):
+                    key_count_str = "0" + key_count_str
+                scene.render.filepath = fpath + fname + "_" + gp_layer_name + "_"
+                scene.render.filepath += str(key_count_str)
+                if gp_layers[gp_layer_name].frames[key_count_blend].strokes.items():
+                    bpy.ops.render.opengl(animation=False, sequencer=False, write_still=True, view_context=True)
+                    key_count_sheet += 1
+                key_count_blend += 1
 
 class ExportKey(bpy.types.Operator):
     
@@ -57,36 +83,6 @@ class ExportKey(bpy.types.Operator):
         
         scene.render.resolution_x = bpy.data.objects['Sheet'].scale[0] * dpi * inch_ratio * 100
         scene.render.resolution_y = bpy.data.objects['Sheet'].scale[1] * dpi * inch_ratio * 100
-        
-        def key_export(gp_layer_name):
-            scene.frame_set(scene.frame_start-1)
-            
-            key_count_blend = 0
-            key_count_sheet = 1
-            
-            while True:
-                if bpy.ops.screen.keyframe_jump(key_jump_next) == {'CANCELLED'}:
-                    break
-                    
-                key_count_str = str(key_count_sheet)
-                
-                if (key_count_sheet < 0) or (key_count_sheet > 9999):
-                    print("Error in key_export: too much keyframes. Maximum count of each cel's keyframe is 9999.")
-                
-                if(key_count_sheet < 10):
-                    key_count_str = "000" + key_count_str
-                if(10 <= key_count_sheet < 100):
-                    key_count_str = "00" + key_count_str
-                if(100 <= key_count_sheet < 1000):
-                    key_count_str = "0" + key_count_str
-                scene.render.filepath = fpath + fname + "_" + gp_layer_name + "_"
-                scene.render.filepath += str(key_count_str)
-                if gp_layers[gp_layer_name].frames[key_count_blend].strokes.items():
-                    bpy.ops.render.opengl(animation=False, sequencer=False, write_still=True, view_context=True)
-                    key_count_sheet += 1
-                key_count_blend += 1
-                
-                
         
         scene.frame_set(scene.frame_start)
         
