c72da91877b9754239da580e5793ee543c6c2768
sus-unn <36620824+sus-unn@users.noreply.github.com>
2018-04-06 22:56:36 +0900

    Version change 0.006.001d dev branch alpha

--- __init__.py
+++ __init__.py
@@ -3,9 +3,9 @@
 
 
 bl_info = { \
-    'name': 'BL2D: Blender 2D Animation toolkit for digital hand-drawn animation (ui-dev branch)',
+    'name': 'BL2D: Blender 2D Animation toolkit for digital hand-drawn animation (dev)',
     'author': 'Susunn',
-    'version': (0, 5, 2),
+    'version': (0, 6, 1),
     'blender': (2, 79, 0),
     'location': 'Toolshelf > Tools',
     'description': 'Make 2D animation in blender. This add-on and template file is yet uncomplete.',
