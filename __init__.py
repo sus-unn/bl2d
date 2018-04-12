b30d15ebb7275214c9849efba22b1a82b47576b8
sus-unn <36620824+sus-unn@users.noreply.github.com>
2018-04-12 21:40:17 +0900

    Add GPL License block

--- __init__.py
+++ __init__.py
@@ -1,6 +1,22 @@
-
-
-
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+# Contributors:
+# Sus-unn
 
 bl_info = { \
     'name': 'BL2D: Blender 2D Animation toolkit for digital hand-drawn animation (alpha)',
@@ -27,6 +43,7 @@ else:
     
 import os
 import bpy
+
 import bpy.utils.previews
 from bpy.types import Menu, Panel, UIList
 
